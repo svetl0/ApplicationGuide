7f62e883cc4299520c561ad72d61d2055553b2ba
svetl0 <Svetlove08@yandex.ru>
2024-06-01 11:36:02 +0300

    last

--- app/src/main/java/com/example/myapplicationguide/InfoActivity.java
+++ app/src/main/java/com/example/myapplicationguide/InfoActivity.java
@@ -86,60 +86,45 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     private void loadImages() {
-        // getting data from our collection and after
-        // that calling a method for on success listener.
+
         db.collection("slider").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-                // inside the on success method we are running a for loop
-                // and we are getting the data from Firebase Firestore
                 for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
 
-                    // after we get the data we are passing inside our object class.
                     SliderData sliderData = documentSnapshot.toObject(SliderData.class);
                     SliderData model = new SliderData();
 
-                    // below line is use for setting our
-                    // image url for our modal class.
                     model.setImgUrl(sliderData.getImgUrl());
 
-                    // after that we are adding that
-                    // data inside our array list.
+
                     sliderDataArrayList.add(model);
 
-                    // after adding data to our array list we are passing
-                    // that array list inside our adapter class.
+
                     adapter = new SliderAdapter(InfoActivity.this, sliderDataArrayList);
 
-                    // below line is for setting adapter
-                    // to our slider view
+
                     sliderView.setSliderAdapter(adapter);
 
-                    // below line is for setting animation to our slider.
+
                     sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
 
-                    // below line is for setting auto cycle duration.
                     sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
 
-                    // below line is for setting
-                    // scroll time animation
+
                     sliderView.setScrollTimeInSec(3);
 
-                    // below line is for setting auto
-                    // cycle animation to our slider
                     sliderView.setAutoCycle(true);
 
-                    // below line is use to start
-                    // the animation of our slider view.
+
                     sliderView.startAutoCycle();
                 }
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                // if we get any error from Firebase we are
-                // displaying a toast message for failure
+
                 Toast.makeText(InfoActivity.this, "Fail to load slider data..", Toast.LENGTH_SHORT).show();
             }
         });
